7aee26f9b01d686f49317667e42f44f9e0116639
Alcides Augusto Bezerra Neto <alcidesneto@consiste.com.br>
2021-02-05 12:12:06 -0300

    First Commit

---
+++ src/test/java/schedulertasks/scheduler/tasks/SchedulerTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package schedulertasks.scheduler.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
